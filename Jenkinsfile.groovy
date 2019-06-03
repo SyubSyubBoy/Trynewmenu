0e8b9cdcc3962ebfb7a0749be0847931bbab09b0
minchanKim <syubsyubboy@gmail.com>
2019-06-03 12:00:16 +0900

    Edit Jenkinsfile (1)

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,4 +1,11 @@
-stage('clone') {
-    echo 'clone start'
-    git 'git@github.com:SyubSyubBoy/Trynewmenu.git'
+node {
+    stage('clone') {
+        echo 'clone start'
+        git 'git@github.com:SyubSyubBoy/Trynewmenu.git'
+    }
+    dir('project') {
+        state('start') {
+            echo 'build start'
+        }
+    }
 }
