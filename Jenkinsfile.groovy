d70a3a5a23c7fc989c6aa5ae9f5f1664f7e8bf8d
minchanKim <syubsyubboy@gmail.com>
2019-06-04 09:51:30 +0900

    Remove Jenkinsfile

--- Jenkinsfile.groovy
+++
@@ -1,9 +0,0 @@
-node {
-    stage('clone') {
-        echo 'clone start'
-        git url: 'git@github.com:SyubSyubBoy/Trynewmenu.git', branch: 'master', credentialsId: 'github-auth'
-    }
-    stage('build') {
-        sh './gradlew clean build'
-    }
-}
